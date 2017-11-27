9857ccb2cc02ccb2edbeac1e4abc0a55ebc52242
AD\mmahmood31 <musasmahmood@gmail.com>
2017-11-26 21:32:34 -0500

    Updated .gitignore, data_analysis.m for PSD analysis, reverting to old mamem CNN model. Updated data extraction process to not mix/overlap windows.

--- Tensorflow/CNN_basic_mamem.py
+++ Tensorflow/CNN_basic_mamem.py
@@ -19,10 +19,9 @@ from tensorflow.python.tools import freeze_graph
 from tensorflow.python.tools import optimize_for_inference_lib
 
 # CONSTANTS:
-# TODO: SELECT 8 CHANNELS: SEE IMAGE:
-# SPECIFIC_CHANNEL_SELECTION = [114, 116, 126, 150, 168]
-SPECIFIC_CHANNEL_SELECTION = range(256)
-NUMBER_CHANNELS_SELECT = np.asarray(SPECIFIC_CHANNEL_SELECTION).shape[0]  # Selects first int in shape
+SPECIFIC_CHANNEL_SELECTION = np.asarray(range(113, 132))
+# SPECIFIC_CHANNEL_SELECTION = np.asarray([120, 121, 122, 123, 124, 125, 126, 138, 149, 158, 167, 175, 187])
+NUMBER_CHANNELS_SELECT = SPECIFIC_CHANNEL_SELECTION.shape[0]  # Selects first int in shape
 
 VERSION_NUMBER = 'v0.0.1'
 DATA_FOLDER_PATH = r'/DATA/output_csv/S011'
@@ -30,10 +29,10 @@ KEY_DATA_DICTIONARY = 'relevant_data'
 EXPORT_DIRECTORY = 'model_exports/' + VERSION_NUMBER + '/'
 MODEL_NAME = 'ssvep_net_14ch'
 NUMBER_STEPS = 5000
-TRAIN_BATCH_SIZE = 256
-VAL_BATCH_SIZE = 20
-DATA_WINDOW_SIZE = 125
-MOVING_WINDOW_SHIFT = 60
+TRAIN_BATCH_SIZE = 64
+VAL_BATCH_SIZE = 32
+DATA_WINDOW_SIZE = 300
+MOVING_WINDOW_SHIFT = 50
 
 NUMBER_CHANNELS_TOTAL = 256
 NUMBER_CLASSES = 5
@@ -64,20 +63,68 @@ def get_data_directory():
     return os.path.abspath(os.curdir) + DATA_FOLDER_PATH
 
 
+def moving_window(data, length, step):
+    # Prepare windows of 'length'
+    streams = it.tee(data, length)
+    # Use step of step, but don't skip any (overlap)
+    return zip(*[it.islice(stream, i, None, step) for stream, i in zip(streams, it.count(step=1))])
+
+
+def separate_data(input_data):
+    data_window_list = list(moving_window(input_data, DATA_WINDOW_SIZE, MOVING_WINDOW_SHIFT))
+    shape = np.asarray(data_window_list).shape
+    # print("dataWindowList.shape (windows, window length, columns)", shape)
+    x_list = []
+    y_list = []
+    for data_window in data_window_list:
+        data_window_array = np.asarray(data_window)
+        count_match = np.count_nonzero(data_window_array[:, NUMBER_CHANNELS_TOTAL] ==
+                                       data_window_array[0, NUMBER_CHANNELS_TOTAL])
+        if count_match == shape[1]:
+            x_window = data_window_array[:, SPECIFIC_CHANNEL_SELECTION]  # [0:2:1]
+            # USE SAME FILTER AS IN ANDROID (C++ filt params),
+            # Will need to pass through that filter in Android before feeding to model.
+            mm_scale = preprocessing.MinMaxScaler().fit(x_window)
+            x_window = mm_scale.transform(x_window)
+            x_list.append(x_window)
+            y_list.append(data_window_array[0, NUMBER_CHANNELS_TOTAL])
+
+    # get unique class values and convert to dummy values
+    # convert lists to arrays; convert to 32-bit floating point
+    # TODO: DO THIS ELSEWHERE:
+    # y_array = np.asarray(pd.get_dummies(y_list).values).astype(np.float32)
+    y_array = np.asarray(y_list)
+    # print("y_array.shape", y_array.shape)
+    x_array = np.asarray(x_list).astype(np.float32)
+    # print("x_array.shape", x_array.shape)
+    return x_array, y_array
+
+
 def load_data(data_directory, letters):
-    data_array = np.empty([0, NUMBER_CHANNELS_TOTAL + 1], np.float32)
+    x_train_data = np.empty([0, DATA_WINDOW_SIZE, NUMBER_CHANNELS_SELECT], np.float32)
+    y_train_data = np.empty([0], np.float32)
+    # data_array = np.empty([0, NUMBER_CHANNELS_TOTAL + 1], np.float32)
     for s in letters:
         str_file_path = data_directory + "/*" + s + "*.mat"
         print(str_file_path)
         training_files = glob.glob(str_file_path)
-        for f in training_files:
+        # TODO: KEEP ONLY TRIALS 9:23:
+        # print("training_files: ", training_files[8:24])
+        for f in training_files[8:24]:
             data_from_file = loadmat(f)  # Saved as mat_dict: Dictionary with variable names as keys:
             # Extract 'relevant_data':
             relevant_data = data_from_file.get(KEY_DATA_DICTIONARY)
-            if data_array.shape[1] == relevant_data.shape[1]:
-                data_array = np.concatenate((data_array, relevant_data), axis=0)
-            print(data_array.shape)
-    return data_array
+            # if data_array.shape[1] == relevant_data.shape[1]:
+            #     data_array = np.concatenate((data_array, relevant_data), axis=0)
+            # print(data_array.shape)
+            x, y = separate_data(relevant_data)
+            x_train_data = np.concatenate((x_train_data, x), axis=0)
+            y_train_data = np.concatenate((y_train_data, y), axis=0)
+    # return data_array
+    print("X_DATA shape: ", x_train_data.shape)
+    y_train_data = np.asarray(pd.get_dummies(y_train_data).values).astype(np.float32)
+    print("Y_DATA shape: ", y_train_data.shape)
+    return x_train_data, y_train_data
 
 
 def model_input(input_node_name, keep_prob_node_name):
@@ -117,46 +164,54 @@ def max_pool_2x2(x, ksize, stride):
 def build_model(x, keep_prob, y, output_node_name):
     x_input = tf.reshape(x, [-1, DATA_WINDOW_SIZE, NUMBER_CHANNELS_SELECT, 1])  # [-1, 250, 256, 1]
 
-    # weight and bias of first convolution layer
-    W_conv1 = weight_variable([5, 5, 1, 32])
-    b_conv1 = bias_variable([32])
-
     # first convolution and pooling
-    h_conv1 = tf.nn.relu(conv2d(x_input, W_conv1) + b_conv1)
-    h_pool1 = max_pool_2x2(h_conv1)
-
-    # weight and bias of the second convolution layer
-    W_conv2 = weight_variable([5, 5, 32, 64])
-    b_conv2 = bias_variable([64])
-
+    w_conv1 = weight_variable(WEIGHT_VAR_CL1)
+    b_conv1 = bias_variable([BIAS_VAR_CL1])
+    # tf.nn.relu: Computes rectified linear: max(features, 0)
+    # Args: features (a tensor)
+    print("x_input.shape", x_input.shape)
+    print("W_conv1.shape", w_conv1.shape)
+    h_conv1 = tf.nn.relu(conv2d(x_input, w_conv1) + b_conv1)
+    print("1st conv layer dimensions: ", h_conv1.shape)
+    # Performs max pooling on the inputs.
+    h_pool1 = max_pool_2x2(h_conv1, MAX_POOL_KSIZE, MAX_POOL_STRIDE)
+    print("h_pool1 output", h_pool1.shape)
     # second convolution and pooling
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-    h_pool2 = max_pool_2x2(h_conv2)
+    w_conv2 = weight_variable(WEIGHT_VAR_CL2)
+    b_conv2 = bias_variable([BIAS_VAR_CL2])
 
-    # weight and bias of the fully connected layer
-    W_fc1 = weight_variable([13 * 256 * 64, 1024])
-    b_fc1 = bias_variable([1024])
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2, MAX_POOL_KSIZE, MAX_POOL_STRIDE)
+
+    # fully connected layer1,the shape of the patch should be defined
+    w_fc1 = weight_variable(WEIGHT_VAR_FC1)
+    b_fc1 = bias_variable(BIAS_VAR_FC1)
 
     # the input should be shaped/flattened
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 13 * 256 * 64])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    h_pool2_flat = tf.reshape(h_pool2, MAX_POOL_FLAT_SHAPE_FC1)
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, w_fc1) + b_fc1)
 
-    keep_prob = tf.placeholder(tf.float32)
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    # fully connected layer2
+    w_fc2 = weight_variable(WEIGHT_VAR_FC2)
+    b_fc2 = bias_variable(BIAS_VAR_FC2)
+    h_fc2 = tf.nn.relu(tf.matmul(h_fc1, w_fc2) + b_fc2)
+
+    h_fc2_drop = tf.nn.dropout(h_fc2, keep_prob)
 
     # weight and bias of the output layer
-    W_fo = weight_variable([1024, 3])
-    b_fo = bias_variable([3])
+    w_fc_output = weight_variable(WEIGHT_VAR_FC_OUTPUT)
+    b_fc_output = bias_variable(BIAS_VAR_FC_OUTPUT)
 
-    y_conv = tf.matmul(h_fc1_drop, W_fo) + b_fo
+    y_conv = tf.matmul(h_fc2_drop, w_fc_output) + b_fc_output
+    outputs = tf.nn.softmax(y_conv, name=output_node_name)
 
-    # training and reducing the cost/loss fucntion
+    # training and reducing the cost/loss function
     cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=y_conv))
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y, 1))
+    correct_prediction = tf.equal(tf.argmax(outputs, 1), tf.argmax(y, 1))
 
-    # finding accuracy
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
     merged_summary_op = tf.summary.merge_all()
 
     return train_step, cross_entropy, accuracy, merged_summary_op
@@ -168,46 +223,11 @@ def test(sess, accuracy, x, y, x_test_val, y_test_val, keep_prob):
                                                              y: y_test_val[0:128], keep_prob: 1.0}))
 
 
-def moving_window(data, length, step):
-    # Prepare windows of 'length'
-    streams = it.tee(data, length)
-    # Use step of step, but don't skip any (overlap)
-    return zip(*[it.islice(stream, i, None, step) for stream, i in zip(streams, it.count(step=1))])
-
-
-def separate_data(input_data):
-    data_window_list = list(moving_window(input_data, DATA_WINDOW_SIZE, MOVING_WINDOW_SHIFT))
-    shape = np.asarray(data_window_list).shape
-    print("dataWindowList.shape (windows, window length, columns)", shape)
-    x_list = []
-    y_list = []
-    for data_window in data_window_list:
-        data_window_array = np.asarray(data_window)
-        count_match = np.count_nonzero(data_window_array[:, NUMBER_CHANNELS_TOTAL] ==
-                                       data_window_array[0, NUMBER_CHANNELS_TOTAL])
-        if count_match == shape[1]:
-            x_window = data_window_array[:, SPECIFIC_CHANNEL_SELECTION]  # [0:2:1]
-            # USE SAME FILTER AS IN ANDROID (C++ filt params),
-            # Will need to pass through that filter in Android before feeding to model.
-            mm_scale = preprocessing.MinMaxScaler().fit(x_window)
-            x_window = mm_scale.transform(x_window)
-            x_list.append(x_window)
-            y_list.append(data_window_array[0, NUMBER_CHANNELS_TOTAL])
-
-    # get unique class values and convert to dummy values
-    # convert lists to arrays; convert to 32-bit floating point
-    y_array = np.asarray(pd.get_dummies(y_list).values).astype(np.float32)
-    print("y_array.shape", y_array.shape)
-    x_array = np.asarray(x_list).astype(np.float32)
-    print("x_array.shape", x_array.shape)
-    return x_array, y_array
-
-
-def train_and_test(training_data, test_data, x, keep_prob, y, train_step, accuracy, saver):
+def train_and_test(x_train_data, y_train_data, x, keep_prob, y, train_step, accuracy, saver):
     val_step = 0
     # split into data windows & store:
-    x_test_data, y_test_data = separate_data(training_data)
-    x_train, x_test, y_train, y_test = train_test_split(x_test_data, y_test_data, train_size=0.5, random_state=1)
+    # x_test_data, y_test_data = separate_data(training_data)
+    x_train, x_test, y_train, y_test = train_test_split(x_train_data, y_train_data, train_size=0.8, random_state=1)
     print("x_train.shape", x_train.shape)
     print("x_test.shape", x_test.shape)
     print("y_train.shape", y_train.shape)
@@ -215,7 +235,6 @@ def train_and_test(training_data, test_data, x, keep_prob, y, train_step, accura
     init_op = tf.global_variables_initializer()
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
-    # sess = tf.Session(config=config)
     with tf.Session(config=config) as sess:
         sess.run(init_op)
         # save model as pbtxt:
@@ -255,10 +274,8 @@ def train_and_test(training_data, test_data, x, keep_prob, y, train_step, accura
         saver.save(sess, EXPORT_DIRECTORY + MODEL_NAME + '.ckpt')
 
         # run Test:
-        # Prepare test_data:
-        x_test_data, y_test_data = separate_data(test_data)
-        print("Validation Accuracy:",
-              sess.run(accuracy, feed_dict={x: x_test_data[0:128], y: y_test_data[0:128], keep_prob: 1.0}))
+        # print("Validation Accuracy:",
+        #       sess.run(accuracy, feed_dict={x: x_test_data[0:128], y: y_test_data[0:128], keep_prob: 1.0}))
 
 
 def export_model(input_node_names, output_node_name):
@@ -290,28 +307,18 @@ def main():
     input_node_name = 'input'
     keep_prob_node_name = 'keep_prob'
     output_node_name = 'output'
-
     x, keep_prob, y_ = model_input(input_node_name, keep_prob_node_name)
-
     train_step, loss, accuracy, merged_summary_op = build_model(x, keep_prob, y_, output_node_name)
-
     saver = tf.train.Saver()
-
     data_directory = get_data_directory()
-
-    training_data = load_data(data_directory, ['a', 'b', 'c', 'd'])
-    test_data = load_data(data_directory, ['e'])
-
-    train_and_test(training_data, test_data, x, keep_prob, y_, train_step, accuracy, saver)
-
+    x_train_data, y_train_data = load_data(data_directory, ['a'])
+    # x_test_data, y_test_data = load_data(data_directory, ['e'])
+    train_and_test(x_train_data, y_train_data, x, keep_prob, y_, train_step, accuracy, saver)
     user_input = input('Export Current Model?')
-
     if user_input == "1" or user_input.lower() == "y":
         export_model([input_node_name, keep_prob_node_name], output_node_name)
-
-    print("TrainD shape: ", training_data.shape)
-    print("TestD shape: ", test_data.shape)
-
+    # print("TrainD shape: ", training_data.shape)
+    # print("TestD shape: ", test_data.shape)
     print("Terminating...")
 
 
--- data_analysis.m
+++ data_analysis.m
@@ -1,21 +1,26 @@
 %% MAMEM DATA ANALYSIS:
 clear;clc;close all;
-SELECT_CHANNELS = [114, 116, 126, 150, 168];
+SELECT_CHANNELS = [1:256];
+% SELECT_CHANNELS = [114, 116, 123, 126, 137, 150, 158, 168];
 Fs = 250;
-dr = dir('DATA\output_csv\S001\*.mat');
-plot_data = 1;
-select = 1:512;
-hannWin = hann(256); wlen = 256; h=64; nfft = 2048;
+dr = dir('DATA\output_csv\S011\*.mat');
+plot_data = [];
+select = 1:1250;
+hannWin = hann(1024); wlen = 256; h=64; nfft = 2048;
 K = sum(hamming(wlen, 'periodic'))/wlen; winLim = [0, 45];
-for i = 1:length(dr)
+X = 1:Fs;
+[~,b,a] = customFilt(X,Fs,[1, 120],3);
+i = 1;
+for i = 12 %1:length(dr)
     load([dr(i).folder, '\', dr(i).name])
     select_data = relevant_data(:,SELECT_CHANNELS);
     % Filter data:
+    CLASS = relevant_data(1, 257)
     for ch = 1:length(SELECT_CHANNELS)
         % Filter:
-        f_data(:,ch) = customFilt(select_data(select,ch),Fs,[1.2, 41],3);
+        f_data(:,ch) = filtfilt(b,a,select_data(select,ch));
         %scale from uV to V:
-        f_data(:,ch) = (f_data(:,ch)/1000000);
+%         f_data(:,ch) = (f_data(:,ch)/1000000);
         %rescale to 1
 %         l = -1; u = 1;
 %         minv = min(f_data(:,ch));
@@ -26,20 +31,31 @@ for i = 1:length(dr)
 %             plot(f_data(:,ch));
 %         end
         % PSD
-        [S, freq] = welch_psd(f_data(:,ch),Fs,hannWin);
+        [S(:,ch), freq] = welch_psd(f_data(:,ch),Fs,hannWin);
+        
         % STFT:
-        [S1, f1, t1] = stft( f_data(:,ch), wlen, h, nfft, Fs ); S2 = 20*log10(abs(S1(f1<winLim(2) & f1>winLim(1),:))/wlen/K + 1e-6);
-        if(plot_data)
-            figure(ch)
-            subplot(2,2,[1,2]); plot(freq,S), xlim(winLim);
-            subplot(2,2,[3,4]); imagesc(t1,f1(f1<winLim(2) & f1>winLim(1)),S2),xlim([min(t1) max(t1)]),ylim(winLim);
-            set(gca,'YDir','normal');xlabel('Time, s');ylabel('Frequency, Hz');colormap(jet);
-            cb = colorbar;ylabel(cb, 'Power (db)')
+%         [S1, f1, t1] = stft( f_data(:,ch), wlen, h, nfft, Fs ); S2 = 20*log10(abs(S1(f1<winLim(2) & f1>winLim(1),:))/wlen/K + 1e-6);
+%{
+        if(isempty(plot_data))    
+            figure(1); hold on;
+%             subplot(2,2,[1,2]); 
+            plot(freq,S(:,ch)), xlim(winLim); title([num2str(ch), ' channel']);
+            
+%             subplot(2,2,[3,4]); imagesc(t1,f1(f1<winLim(2) & f1>winLim(1)),S2),xlim([min(t1) max(t1)]),ylim(winLim);
+%             set(gca,'YDir','normal');xlabel('Time, s');ylabel('Frequency, Hz');colormap(jet);
+%             cb = colorbar;ylabel(cb, 'Power (db)')
         end
+        
+        if (mod(ch,16) == 0)
+            plot_data = input('Continue? \n');
+            clf(1)
+        end
+%}
     end
-    plot_data = input('Continue? \n');
-    if(isempty(plot_data))
-        plot_data = 1;
-    end
-    clf([1:5]);
 end
+
+% plot(freq, S(:, 1)); xlim(winLim);
+%% CLASS 1:
+% plot(S(45,:));
+%% CLASS 2:
+plot(S(61,:));
